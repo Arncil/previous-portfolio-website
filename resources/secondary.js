fa4129089cefcbfe1e6da22a03e7ee2581fbbe2f
Arncil <jackowiakmilosz@gmail.com>
2022-11-20 10:01:35 +0100

    Add new pages and part of contact page

--- resources/secondary.js
+++ resources/secondary.js
@@ -13,19 +13,3 @@ hideMenu.addEventListener('click', () => {
     showMenu.style.display = 'block';
     hideMenu.style.display = 'none';
 });
-
-const showMore = document.getElementById('character-show-more');
-const showLess = document.getElementById('character-show-less');
-const characterDescription = document.getElementById('character-description');
-
-showMore.addEventListener('click', () => {
-    showMore.style.display = 'none';
-    characterDescription.style.display = 'grid';
-    showLess.style.display = 'grid';
-});
-
-showLess.addEventListener('click', () => {
-    showMore.style.display = 'grid';
-    characterDescription.style.display = 'none';
-    showLess.style.display = 'none';
-});
