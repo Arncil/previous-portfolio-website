ba126a8d4576fc59ddd7f84e08f8fddf2e56a246
Arncil <jackowiakmilosz@gmail.com>
2022-12-14 06:35:55 +0100

    Add collection page

---
+++ resources/javascript/test.js
@@ -0,0 +1,9 @@
+const howOld = (age, year) => { // 25, 2030 / 25, 2020 / 25, 1990
+    const currentYear = 2022
+    // calculatedAge podaje ile lat osoba ma w roku year
+    const calculatedAge = year - currentYear + age // 33 / 23 / -7
+
+    if (calculatedAge > age) {}
+    if else (calculatedAge < 0) {}
+    else { return ``}
+}
